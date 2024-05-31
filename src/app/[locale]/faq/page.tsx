bb15f7586b2d2b2221ed520605a5ebed6b4ab1b5
mohamad-n <alfymohamad@gmail.com>
2024-05-31 17:21:50 +0330

    chore: update faq max width

--- src/app/[locale]/faq/page.tsx
+++ src/app/[locale]/faq/page.tsx
@@ -5,7 +5,7 @@ import initTranslations from "@/app/i18n";
 const FAQ = async ({ params: { locale } }: { params: { locale: string } }) => {
   const { t } = await initTranslations(locale, ["faq"]);
   return (
-    <div className="container px-5 md:px-0 flex flex-col gap-y-[30px] my-[50px]">
+    <div className="container px-5 md:px-0 flex flex-col gap-y-[30px] my-[50px] max-w-4xl">
       {[...Array(8).keys()].map((idx) => (
         <FaqItem
           key={idx}
